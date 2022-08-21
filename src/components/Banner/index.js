8ce7bf8c474faefd7d5b577cf149859ef02f33f9
Amelie Pira <amelie.pira@gmail.com>
2022-08-21 21:06:37 +0100

    adjusted navlinks alignment

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,12 +1,12 @@
 export const Banner = () => {
   return (
     <div>
-      <div className="navbar">
-        <div className="navbar-title">
-          <h1 className="title">Banner</h1>
+      <div className="about">
+        <div className="about-title">
+          <h1 className="title">About me</h1>
         </div>
       </div>
-      <div className="banner"></div>
+      <div className="skills"></div>
     </div>
   );
 };
