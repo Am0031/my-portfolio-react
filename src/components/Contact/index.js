67936f758c84628b731bf802b3b50f693f65111c
Amelie Pira <amelie.pira@gmail.com>
2022-08-21 19:42:35 +0100

    adjusted form offset

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -13,78 +13,80 @@ export const Contact = () => {
   return (
     <div>
       <h1 className="title">Contact me</h1>
-      <Form
-        name="basic"
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 16,
-        }}
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Full Name"
-          name="fullname"
-          rules={[
-            {
-              required: true,
-              message: "Please input your full name!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Subject"
-          name="subject"
-          rules={[
-            {
-              required: true,
-              message: "Please input a subject!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Message"
-          name="message"
-          rules={[
-            {
-              required: true,
-              message: "Please input your message here!",
-            },
-          ]}
-        >
-          <TextArea rows={4} />
-        </Form.Item>
-        <Form.Item
-          name="email"
-          valuePropName="checked"
-          wrapperCol={{
-            offset: 8,
-            span: 16,
+      <div className="form-control">
+        <Form
+          name="basic"
+          labelCol={{
+            span: 4,
           }}
-        >
-          <Checkbox>Email me a copy</Checkbox>
-        </Form.Item>
-        <Form.Item
           wrapperCol={{
-            offset: 8,
-            span: 16,
+            span: 18,
           }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Button type="primary" htmlType="submit">
-            Send
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item
+            label="Full Name"
+            name="fullname"
+            rules={[
+              {
+                required: true,
+                message: "Please input your full name!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Subject"
+            name="subject"
+            rules={[
+              {
+                required: true,
+                message: "Please input a subject!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Message"
+            name="message"
+            rules={[
+              {
+                required: true,
+                message: "Please input your message here!",
+              },
+            ]}
+          >
+            <TextArea rows={4} />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 4,
+              span: 16,
+            }}
+          >
+            <Checkbox>Email me a copy</Checkbox>
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              offset: 4,
+              span: 24,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Send
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
