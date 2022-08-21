410850b94e7fa6094bc6e164c7cd0575fe075f21
Amelie Pira <amelie.pira@gmail.com>
2022-08-21 19:31:10 +0100

    added language dropdown

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,24 +1,63 @@
-import { Menu } from "antd";
+import { useEffect, useState } from "react";
+import { DownOutlined } from "@ant-design/icons";
+import { Dropdown, Menu, Space, Typography } from "antd";
 
-const items = [
-  { label: "item 1", key: "item-1" }, // remember to pass the key prop
-  { label: "item 2", key: "item-2" }, // which is required
+const linkItems = [
+  { label: "link 1", key: "link-1" }, // remember to pass the key prop
+  { label: "link 2", key: "link-2" }, // which is required
   {
     label: "sub menu",
     key: "submenu",
-    children: [{ label: "item 3", key: "submenu-item-1" }],
+    children: [
+      { label: "sublink 1", key: "submenu-link-1" },
+      { label: "sublink 2", key: "submenu-link-2" },
+    ],
   },
 ];
 
 export const Navbar = () => {
+  const [itemName, setItemName] = useState("English");
+
+  const languageItems = [
+    {
+      key: "1",
+      label: "English",
+    },
+    {
+      key: "2",
+      label: "Francais",
+    },
+    {
+      key: "3",
+      label: "Deutsch",
+    },
+  ];
+  const languageMenu = (
+    <Menu
+      selectable
+      defaultSelectedKeys={["1"]}
+      items={languageItems}
+      onSelect={({ key }) => {
+        setItemName(languageItems[key - 1].label);
+      }}
+    />
+  );
+
   return (
     <div className="navbar">
       <div className="navTitle">
         <h1 className="title">My Portfolio</h1>
-        <h2 className="title">Language select</h2>
+        <Dropdown overlay={languageMenu}>
+          <Typography.Link>
+            <Space>
+              {itemName}
+              <DownOutlined />
+            </Space>
+          </Typography.Link>
+        </Dropdown>
       </div>
       <div className="navLink">
-        <Menu mode="horizontal" items={items} />
+        <Menu mode="horizontal" items={linkItems} />
       </div>
     </div>
   );
