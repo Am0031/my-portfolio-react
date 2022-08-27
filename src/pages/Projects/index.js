23534806b6d500c04fbe0bd7b424dbd15cb95881
Amelie Pira <amelie.pira@gmail.com>
2022-08-27 18:04:19 +0100

    added projects page title translation

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,15 +1,17 @@
+import { useTranslation } from "react-i18next";
 import projectData from "../../data/projects.json";
 import { ProjectCard } from "../../components/Card";
 import { Badge } from "antd";
 
 export const Projects = () => {
+  const { t } = useTranslation();
   const show = true;
   const githubUser = "Am0031";
 
   return (
     <div className="main projects-container">
       <div className="project-cards-title">
-        <h1 className="title">My projects</h1>
+        <h1 className="title">{t("projects-title")}</h1>
         <Badge
           count={show ? projectData.projects.length : 0}
           style={{ backgroundColor: "#531dab" }}
