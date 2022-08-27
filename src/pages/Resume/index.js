fc3559d95da49c7bfadaa9278693cafa05a853d8
Amelie Pira <amelie.pira@gmail.com>
2022-08-27 19:49:03 +0100

    added resume items translations

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,15 +1,17 @@
 import { Button } from "antd";
+import { useTranslation } from "react-i18next";
 
 export const Resume = () => {
+  const { t } = useTranslation();
   return (
     <div className="resume-container main">
-      <h1 className="title resume-title">My CV</h1>
+      <h1 className="title resume-title">{t("resume-title")}</h1>
       <div className="resume-control">
         <Button
           style={{ backgroundColor: "#531dab", color: "#fff", margin: 10 }}
         >
           <a href={`/resume.pdf`} target="_blank" rel="noreferrer">
-            Download Pdf
+            {t("download-resume")}
           </a>
         </Button>
       </div>
