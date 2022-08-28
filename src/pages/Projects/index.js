3f0e32ed69e3ce31c5946d31b0c3efb33511c549
Amelie Pira <amelie.pira@gmail.com>
2022-08-28 16:50:18 +0100

    imported images in projects.js

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import projectData from "../../data/projects.json";
+import projects from "../../data/projects";
 import { ProjectCard } from "../../components/Card";
 import { Badge } from "antd";
 
@@ -12,12 +12,12 @@ export const Projects = () => {
       <div className="project-cards-title">
         <h1 className="title">{t("projects-title")}</h1>
         <Badge
-          count={show ? projectData.projects.length : 0}
+          count={show ? projects.length : 0}
           style={{ backgroundColor: "#531dab" }}
         />
       </div>
       <div className="project-cards-container">
-        {projectData.projects.map((item) => {
+        {projects.map((item) => {
           return <ProjectCard data={item} key={item.name} />;
         })}
       </div>
